aa7116fe8b785c37ce88829402750255f8862a0a
GaloMegg <galomeggiolarobul@gmail.com>
2024-03-14 22:15:09 -0300

    fix(clients): :card_file_box: database added to client

--- src/clients/firebase.app.ts
+++ src/clients/firebase.app.ts
@@ -4,6 +4,7 @@ import {getFirestore} from 'firebase/firestore';
 export const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_AUTHDOMAIN,
   projectId: process.env.FIREBASE_PROJECTID,
   storageBucket: process.env.FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
