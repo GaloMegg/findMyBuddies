b345aab918e917350a0894d99e421686b327491c
GaloMegg <galomeggiolarobul@gmail.com>
2024-04-11 10:27:53 -0300

    fix(multiscope): :fire: remove icons component and added icons as files

--- src/utils/icons.tsx
+++
@@ -1,5 +0,0 @@
-export const ICONS = {
-  CAT: () => require('../assets/icons/buddiestype/cat.svg'),
-  DOG: () => require('../assets/icons/buddiestype/cat.svg'),
-  OTHER: () => require('../assets/icons/buddiestype/other.svg'),
-};
